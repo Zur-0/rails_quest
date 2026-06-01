4280066d650196637f432ae55538c09b9e120a9d
Zur-0 <Egor_Med@outlook.com>
2026-06-01 23:52:39 +0300

    second quest

--- app/services/quest2_student_service.rb
+++ app/services/quest2_student_service.rb
@@ -2,32 +2,48 @@ class Quest2StudentService
   class << self
     # @return [String]
     def all_agents
-      ""
+      Agent.order(:codename).pluck(:codename).join("\n")
     end
 
     # @return [String]
     def all_missions
-      ""
+      Mission.order(:title).pluck(:title).join("\n")
     end
 
     # @return [String]
     def agents_with_missions
-      ""
+       Agent
+    .includes(:missions)
+    .order(:codename)
+    .map { |agent| "#{agent.codename}: #{agent.missions.order(:title).pluck(:title).join(', ')}" }
+    .join("\n")
     end
 
     # @return [String]
     def agents_with_missions_sorted_by_mission_count
-      ""
+       Agent
+    .includes(:missions)
+    .sort_by { |agent| [-agent.missions.size, agent.codename] }
+    .map { |agent| "#{agent.codename} (#{agent.missions.size}): #{agent.missions.order(:title).pluck(:title).join(', ')}" }
+    .join("\n")
     end
 
     # @return [String]
     def agents_with_skills
-      ""
+       Agent
+    .includes(:skills)
+    .order(:codename)
+    .map { |agent| "#{agent.codename}: #{agent.skills.order(:name).pluck(:name).join(', ')}" }
+    .join("\n")
     end
 
     # @return [String]
     def skills_by_agent_count
-      ""
+       Skill
+    .includes(:agents)
+    .sort_by { |skill| [-skill.agents.size, skill.name] }
+    .map { |skill| "#{skill.name} (#{skill.agents.size}): #{skill.agents.order(:codename).pluck(:codename).join(', ')}" }
+    .join("\n")
     end
   end
 end
